03529d3bc3479c7e31636c5db5f06faf84479618
Philip Lee <rocketman768@gmail.com>
2013-02-19 11:56:47 -0600

    Texture passing now occurs through extern linkage.

--- Matting.cu
+++ Matting.cu
@@ -4,14 +4,57 @@
  */
 
 #include <cuda.h>
-#include "CompressedMatrix.h"
-#include "CompressedMatrix.cu"
+#include "BandedMatrix.h"
 #include "Vector.cu"
 
-__device__ void levinLaplacian( CompressedMatrix* L, float* b, texture<float,3,cudaReadModeNormalizedFloat> const* image, texture<float,1,cudaReadModeNormalizedFloat> const* trimap )
+/*!
+ * \brief Generate Levin's Laplacian for a given image.
+ * 
+ * NOTE: requires \c levinLaplacian_image to be a globally visible
+ *       texture<float4,cudaTextureType2D,cudaReadModeElementType>
+ * NOTE: requires \c levinLaplacian_trimap to be a globally visible
+ *       texture<float4,cudaTextureType2D,cudaReadModeElementType>
+ * 
+ * \param L output banded sparse Laplacian
+ * \param b output right-hand side.
+ */
+__device__ void levinLaplacian( BandedMatrix* L, float* b )
 {
+   extern texture<float4,cudaTextureType2D,cudaReadModeElementType> levinLaplacian_image;
+   extern texture<float,cudaTextureType2D,cudaReadModeElementType> levinLaplacian_trimap;
+   
    const float gamma = 1e1;
    const int winRad = 1;
+   const int nthreads = blockDim.x*gridDim.x;
+   const int i = blockIdx.x*blockDim.x + threadIdx.x;
+   
+   float u, v;
+   float4 rgba;
+   // Local covariance matrix (symmetric).
+   float c11, c12, c13,
+              c22, c23,
+                   c33;
+   float cdet;
+   // Local inverse of covariance matrix (symmetric).
+   float d11, d12, d13,
+              d22, d23,
+                   d33;
+   while(true)
+   {
+      rgba = tex2D(levinLaplacian_image, u, v);
+      // Get the inverse.
+      cdet = -c11*c12*c12 +
+             c11*c11*c22 -
+             c13*c13*c22 +
+             2*c12*c13*c23 -
+             c11*c23*c23;
+      d11 = (c11*c22 - c23*c23)/cdet;
+      d12 = (c13*c23 - c11*c12)/cdet;
+      d13 = (c12*c23 - c13*c22)/cdet;
+      d22 = (c11*c11 - c13*c13)/cdet;
+      d23 = (c12*c13 - c11*c23)/cdet;
+      d33 = (c11*c22 - c12*c12)/cdet;
+   }
 }
 
 /*
--- main.cu
+++ main.cu
@@ -1,17 +1,12 @@
 #include <stdio.h>
+#include <cuda.h>
 #include "ppm.h"
 #include "Matting.cu"
 
-void help()
-{
-   fprintf(
-      stderr,
-      "Usage: matting <image>.ppm\n"
-      "  image - An RGB image to matte\n"
-   );
-   
-   exit(0);
-}
+texture<float4,cudaTextureType2D,cudaReadModeElementType> levinLaplacian_image;
+texture<float,cudaTextureType2D,cudaReadModeElementType> levinLaplacian_trimap;
+
+void help();
 
 int main(int argc, char* argv[])
 {
@@ -27,3 +22,13 @@ int main(int argc, char* argv[])
    return 0;
 }
 
+void help()
+{
+   fprintf(
+      stderr,
+      "Usage: matting <image>.ppm\n"
+      "  image - An RGB image to matte\n"
+   );
+   
+   exit(0);
+}
