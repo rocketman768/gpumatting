be99c05ce9be00ebaa1208a28135b8ca6d907cc1
Philip Lee <rocketman768@gmail.com>
2013-03-15 08:39:21 -0500

    Added GPL-3 copyright to the code and program.

--- main.cu
+++ main.cu
@@ -1,3 +1,20 @@
+/* main.cu is part of gpumatting and is 
+ * Copyright 2013 Philip G. Lee <rocketman768@gmail.com>
+ * 
+ * gpumatting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * gpumatting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with gpumatting.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <cuda.h>
 #include <time.h>
